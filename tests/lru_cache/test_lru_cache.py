48210b34725003e9d29573fc1f8d1211629278b0
yamap55 <tetuya120@hotmail.com>
2021-09-03 09:37:08 +0900

    [add] get unit test

--- tests/lru_cache/test_lru_cache.py
+++ tests/lru_cache/test_lru_cache.py
@@ -5,3 +5,11 @@ def test_put():
     lru = LastRecentlyUsedCashe()
     lru.put("a", "dataA")
     assert True
+
+
+def test_get():
+    lru = LastRecentlyUsedCashe()
+    lru.put("a", "dataA")
+    actual = lru.get("a")
+    expected = "dataA"
+    assert actual == expected
