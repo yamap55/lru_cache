303e3b042c21c050a100bd4e0c89380c9236449d
yamap55 <tetuya120@hotmail.com>
2021-09-03 09:35:02 +0900

    [add] docstring

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,6 +1,23 @@
+"""
+Last Recently Used Cashe
+"""
 from typing import Any
 
 
 class LastRecentlyUsedCashe:
+    """
+    Last Recently Used Cashe
+    """
+
     def put(self, key: str, value: Any) -> None:
+        """
+        要素を格納する
+
+        Parameters
+        ----------
+        key : str
+            要素のキー
+        value : Any
+            要素の値
+        """
         pass
