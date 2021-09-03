57f1adecd141ce475036ae36570cfe6c4ef2e592
yamap55 <tetuya120@hotmail.com>
2021-09-03 09:43:22 +0900

    [refactor] add empty row

--- tests/lru_cache/test_lru_cache.py
+++ tests/lru_cache/test_lru_cache.py
@@ -4,12 +4,14 @@ from lru_cache.lru_cache import LastRecentlyUsedCashe
 def test_put():
     lru = LastRecentlyUsedCashe()
     lru.put("a", "dataA")
+
     assert True
 
 
 def test_get():
     lru = LastRecentlyUsedCashe()
     lru.put("a", "dataA")
+
     actual = lru.get("a")
     expected = "dataA"
     assert actual == expected
